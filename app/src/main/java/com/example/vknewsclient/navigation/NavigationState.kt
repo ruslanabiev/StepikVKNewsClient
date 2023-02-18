0a5bcccd762aa6bbe02b9aa644b3dede89d65058
Ruslan Abiev <abievruslan@yandex.ru>
2023-02-18 21:03:13 +0300

    ls5_4_finished

---
+++ app/src/main/java/com/example/vknewsclient/navigation/NavigationState.kt
@@ -0,0 +1,31 @@
+package com.example.vknewsclient.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+class NavigationState(
+    val navHostController: NavHostController
+) {
+
+    fun navigateTo(route: String) {
+
+        navHostController.navigate(route) {
+            popUpTo(id = navHostController.graph.startDestinationId) {
+                saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+    }
+}
+
+@Composable
+fun rememberNavigationState(
+   navHostController: NavHostController = rememberNavController()
+): NavigationState {
+   return remember {
+       NavigationState(navHostController)
+   }
+}
